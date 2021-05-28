7e8c5117137ffab46f75d6b83d1ee4ddd3a0b017
Yanndroid <yann.ritzert@yahoo.fr>
2021-05-28 14:32:55 +0200

    Initial commit

---
+++ app/src/main/java/de/dylt/yanndroid/screenresolution/AboutActivity.java
@@ -0,0 +1,104 @@
+package de.dylt.yanndroid.screenresolution;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.appbar.AppBarLayout;
+
+public class AboutActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        initToolbar();
+
+        settilte("");
+
+    }
+
+    public void initToolbar() {
+        /** Def */
+        androidx.appcompat.widget.Toolbar toolbar = (androidx.appcompat.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        AppBarLayout AppBar = findViewById(R.id.app_bar);
+
+        TextView expanded_title = findViewById(R.id.expanded_title);
+        TextView collapsed_title = findViewById(R.id.collapsed_title);
+
+        /** 1/3 of the Screen */
+        ViewGroup.LayoutParams layoutParams = AppBar.getLayoutParams();
+        layoutParams.height = (int) ((double) this.getResources().getDisplayMetrics().heightPixels / 2.6);
+
+
+        /** Collapsing */
+        AppBar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                float percentage = (AppBar.getY() / AppBar.getTotalScrollRange());
+                expanded_title.setAlpha(1 - (percentage * 2 * -1));
+                collapsed_title.setAlpha(percentage * -1);
+            }
+        });
+
+        AppBar.setExpanded(false);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    public void settilte(String title) {
+        TextView expanded_title = findViewById(R.id.expanded_title);
+        TextView collapsed_title = findViewById(R.id.collapsed_title);
+        expanded_title.setText(title);
+        collapsed_title.setText(title);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.app_info, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.ai_settings) {
+            callAppInfo();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void openGithub(View v) {
+        //github todo
+    }
+
+    private void callAppInfo() {
+        try {
+            Intent intent = new Intent("android.settings.APPLICATION_DETAILS_SETTINGS");
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            intent.setData(Uri.parse("package:" + getApplicationContext().getPackageName()));
+            startActivity(intent);
+        } catch (ActivityNotFoundException unused) {
+            startActivity(new Intent("android.settings.MANAGE_APPLICATIONS_SETTINGS"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

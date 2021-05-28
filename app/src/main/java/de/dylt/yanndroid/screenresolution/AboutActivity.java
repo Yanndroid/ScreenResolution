d522000717ee45f32850b3dfc576184ea2a2bbb1
Yanndroid <yann.ritzert@yahoo.fr>
2021-05-28 14:38:36 +0200

    launch from settings

--- app/src/main/java/de/dylt/yanndroid/screenresolution/AboutActivity.java
+++ app/src/main/java/de/dylt/yanndroid/screenresolution/AboutActivity.java
@@ -8,7 +8,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
